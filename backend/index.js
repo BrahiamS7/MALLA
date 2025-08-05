1e3786a219b023980c5f3b7b0bfbbd8f28c77065
BrahiamS7 <bfsoto16@gmail.com>
2025-08-04 22:36:10 -0500

    arreglos en render

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,38 @@
 // IMPORTACIONES
 import express from "express";
-import cors from 'cors'
-import env from 'dotenv'
+import cors from "cors";
+import env from "dotenv";
 
 //IMPORTACIONES DE RUTAS
-import materiasRoutes from './routes/materias.routes.js';
+import materiasRoutes from "./routes/materias.routes.js";
 
 // CONSTANTES
-const app=express();
-const port=process.env.PORT || 3001;
-env.config()
+const app = express();
+const port = process.env.PORT || 3001;
+env.config();
 
 // UTILIDADES
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://malla-brahiams7s-projects.vercel.app"
+      "https://malla-brahiams7s-projects.vercel.app",
+      "https://malla-33ng.onrender.com/",
     ],
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+    credentials: "true",
   })
 );
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // RUTAS
-app.get("/api",(req,res)=>{
-    res.json({msg:"API funcionando correctamente"})
-})
-app.use('/api/materias',materiasRoutes)
-
-
+app.get("/api", (req, res) => {
+  res.json({ msg: "API funcionando correctamente" });
+});
+app.use("/api/materias", materiasRoutes);
 
 //RUTA LISTEN
-app.listen(port,()=>{
-    console.log(`SERVER RUNNING IN PORT ${process.env.PORT}`);
-})
+app.listen(port, () => {
+  console.log(`SERVER RUNNING IN PORT ${process.env.PORT}`);
+});
